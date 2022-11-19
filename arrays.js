5e542b782e818ce8b6db89154c69108917c2430a
Jasmine Cheung <jasminesyc2000@gmail.com>
2022-11-18 23:49:58 -0500

    Problem 1 solved

--- arrays.js
+++ arrays.js
@@ -1,9 +1,10 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
-const faveColors = ['red', 'green', 'black']
+const faveColors = ["red", "green", "black"];
 
 //CODE HERE
+let colorCopy = faveColors.slice(0, 3);
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
@@ -16,10 +17,9 @@ const faveColors = ['red', 'green', 'black']
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
-const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5];
 
 //CODE HERE
-
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -31,6 +31,6 @@ const numbers = [1, 2, 3, 4, 5]
   to the answers array. 
 */
 
-const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
+const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352];
 
 // CODE HERE
