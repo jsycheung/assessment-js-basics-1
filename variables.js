448fc23216477740f6b9fd5fe3c5ab12acfcada9
Jasmine Cheung <jasminesyc2000@gmail.com>
2022-11-18 23:43:43 -0500

    Problem 4 solved

--- variables.js
+++ variables.js
@@ -24,6 +24,11 @@ let lovesCode = true;
 */
 
 //CODE HERE
+if (faveNum === 13) {
+  console.log("lucky 13!");
+} else {
+  console.log("not lucky 13");
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
