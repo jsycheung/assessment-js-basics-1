d2ba4c558df4b9f7ff4cfab3a1987464fb679a52
Jasmine Cheung <jasminesyc2000@gmail.com>
2022-11-18 23:39:55 -0500

    completed

---
+++ sdlc.js
@@ -0,0 +1,31 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning =
+  "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+const analysisOfRequirements =
+  "This is the second step, in which the functional and system requirements of the business process, the user requirements and the operational requirements will be defined. Risks related to how the technology will be integrated into standard operating procedures are also identified.";
+const design =
+  "This is the third step, in which system and software design is prepared from planning and requirement analysis. Specifications such as hardware and system requirements, and overall system architecture, will be defined.";
+const implementation =
+  "This is the fourth step, in which software design is translated into source code by developers once they receive the design document. All components of the software are implemented.";
+const testingAndIntegration =
+  "This is the fifth step, in which the developed software is tested thoroughly. Any defects found are assigned to developers to get them fixed.";
+const maintain =
+  "This is the sixth and final step, in which the product is maintained. If any issues comes up, they will be fixed. The developers will also enhance the software as necessary.";
